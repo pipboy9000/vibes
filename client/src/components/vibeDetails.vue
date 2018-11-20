8eeb8cbbf3ee2130fe0c9425dae667854ca627b2
Johnny <syonip@gmail.com>
2018-11-20 16:50:21 +0200

    free-google-image-search. change to https://cors-anywhere.herokuapp.com

--- client/src/components/vibeDetails.vue
+++ client/src/components/vibeDetails.vue
@@ -102,6 +102,8 @@ const pica = Pica();
 import PulseLoader from "vue-spinner/src/PulseLoader.vue";
 import Logo from "./logo";
 import { PhotoGallery } from 'vue-photo-gallery';
+import GoogleImageSearch from 'free-google-image-search';
+console.dir(GoogleImageSearch);
 
 export default {
   name: "VibeDetails",
@@ -347,6 +349,18 @@ export default {
       this.isMobile = window.innerWidth < 650 ? true : false;
       this.isOpen = true;
       this.resizeLayout();
+
+      this.demoPics = [];
+        if (this.vibe.title) {
+          GoogleImageSearch.searchImage(this.vibe.title).then((res) => {
+            res.forEach(url => {
+              this.demoPics.push({
+              imgUrl: url,
+              thumbnailUrl: url
+            });
+            })
+          }, err => console.error(err))
+        }
     },
     close() {
       this.$router.go(-1);
@@ -396,16 +410,17 @@ export default {
       });
     },
     vibePictures() {
-      if (!this.demoPics) {
-        this.demoPics = [];
-        for (var i=0; i<30; i++) {
-          var url = 'https://picsum.photos/200/300/?random&r=' + Math.random();
-          this.demoPics.push({
-            imgUrl: url,
-            thumbnailUrl: url
-          });
-        }
-      }
+      // if (!this.demoPics) {
+        
+ 
+        // for (var i=0; i<30; i++) {
+        //   var url = 'https://picsum.photos/200/300/?random&r=' + Math.random();
+        //   this.demoPics.push({
+        //     imgUrl: url,
+        //     thumbnailUrl: url
+        //   });
+        // }
+      // }
       var returnPics = this.vibe.pictures.reverse().concat(this.demoPics);
       return returnPics;
     },
