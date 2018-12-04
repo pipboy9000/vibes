bd03008423b56f33e54535af1aa1a3bec8722649
Johnny <syonip@gmail.com>
2018-12-04 09:06:04 +0200

    fixed slider align

--- admin/src/components/DateSlider.vue
+++ admin/src/components/DateSlider.vue
@@ -1,19 +1,16 @@
 <template>
-  <v-container>
+  <v-container >
     <v-layout>
       <v-flex>
-        <v-slider v-model="slider" thumb-label thumb-size="104">
-          <template
-          slot="thumb-label"
-          slot-scope="props"
-        >
-          <div>
-            {{ dateThumb(props.value) }} 
-          </div>
-          <div>
-            {{ timeThumb(props.value) }}
-          </div>
-        </template>
+        <v-slider v-model="slider" thumb-label thumb-size="100">
+          <template slot="thumb-label" slot-scope="props">
+            <v-container>
+            <v-layout class="text-xs-center">
+              {{ dateThumb(props.value) }}
+              {{ timeThumb(props.value) }}
+            </v-layout>
+            </v-container>
+          </template>
         </v-slider>
       </v-flex>
     </v-layout>
@@ -25,32 +22,29 @@ import { EventBus } from "../../../client/src/event-bus.js";
 
 export default {
   name: "DateSlider",
-  components: {
-    
-  },
+  components: {},
   data: () => ({
-    slider: 45,
+    slider: 0,
     ticksLabels: []
   }),
   mounted() {
     let dateNow = new Date();
     let timeStepsBack = 100;
     let timeMult = 1000 * 60 * 60;
-    for (let i=0; i<timeStepsBack; i++) {
-      this.ticksLabels.push(dateNow - (timeStepsBack + i) * timeMult)
+    for (let i = 0; i < timeStepsBack; i++) {
+      this.ticksLabels.push(dateNow - (timeStepsBack + i) * timeMult);
     }
   },
   methods: {
-    dateThumb (val) {
-      return new Date(this.ticksLabels[val]).toDateString()
+    dateThumb(val) {
+      return new Date(this.ticksLabels[val]).toLocaleDateString("he-IL");
     },
-    timeThumb (val) {
-      return new Date(this.ticksLabels[val]).toLocaleTimeString()
-    },
+    timeThumb(val) {
+      return new Date(this.ticksLabels[val]).toLocaleTimeString("he-IL");
+    }
   }
 };
 </script>
 
 <style lang="scss" scoped>
-
 </style>
