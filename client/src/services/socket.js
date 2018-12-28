f6afe17b79097561c8c70c1d7b2f0fb7f1632b1c
Johnny <syonip@gmail.com>
2018-12-28 09:03:34 +0200

    sue me

--- client/src/services/socket.js
+++ client/src/services/socket.js
@@ -5,7 +5,8 @@ import store from "../store";
 var socketAddress = process.env.SOCKET_ADDRESS;
 
 //  var socketAddress = "https://vibes-web.herokuapp.com";
-//var socketAddress = "http://172.16.0.95:8080";
+// var socketAddress = "http://172.16.0.95:8080";
+// var socketAddress = "http://192.168.43.3:8080";
 // var port = 80;
 //socketAdress = socketAdress.replace(port, "8080");
 // var io = socketio.connect(socketAdress);
